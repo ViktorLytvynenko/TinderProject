cff955fda0422fbf12e05c8f7433fe25bb4bb4c5
Eugeneowner <ownereugene@gmail.com>
2024-05-25 17:34:58 +0300

    some fixes

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -3,6 +3,7 @@ package entity;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
 public class User {
     private Long id;
     private String name;
@@ -23,41 +24,51 @@ public class User {
         return id;
     }
 
+
     public void setId(Long id) {
         this.id = id;
     }
+
 
     public String getName() {
         return name;
     }
 
+
     public void setName(String name) {
         this.name = name;
     }
+
 
     public String getPhoto() {
         return photo;
     }
 
+
     public void setPhoto(String photo) {
         this.photo = photo;
     }
+
 
     public String getEmail() {
         return email;
     }
 
+
     public void setEmail(String email) {
         this.email = email;
     }
+
 
     public String getPassword() {
         return password;
     }
 
+
     public void setPassword(String password) {
         this.password = password;
     }
+
 
     public static User resultSetToUser(ResultSet resultSet) throws SQLException {
         User user = new User();
@@ -68,6 +79,7 @@ public class User {
         user.setPassword(resultSet.getString(5));
         return user;
     }
+
 
     @Override
     public String toString() {
